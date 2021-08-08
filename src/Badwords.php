c78554c02a20efe6bd6d0677981a91976abcea2a
Mochammad Masbuchin <buchin@dropsugar.com>
2021-08-09 00:13:51 +0700

    Feature: Bad phrase detect

--- src/Badwords.php
+++ src/Badwords.php
@@ -1,49 +1,76 @@
 <?php namespace Buchin\Badwords;
 
 /**
-* 
-*/
+ *
+ */
 class Badwords
 {
-	public static function isDirty($string)
-	{
-		$words = explode(' ', $string);
+    public static function isDirty($string)
+    {
+        $words = explode(" ", $string);
 
-		$bad_words = self::getBadWords();
+        $bad_words = self::getBadWords();
+        $bad_phrases = self::getBadPhrases();
 
-		foreach ($words as $word) {
-			if(in_array(strtolower($word), $bad_words)){
-				return true;
-			}
-		}
+        foreach ($bad_phrases as $bad_phrase) {
+            if (strpos($string, $bad_phrase) !== false) {
+                return true;
+            }
+        }
 
-		return false;
-	}
+        foreach ($words as $word) {
+            if (in_array(strtolower($word), $bad_words)) {
+                return true;
+            }
+        }
 
-	public static function strip($string)
-	{
-		$words = explode(' ', $string);
+        return false;
+    }
 
-		$bad_words = self::getBadWords();
+    public static function strip($string)
+    {
+        $words = explode(" ", $string);
 
-		$new_words = [];
+        $bad_words = self::getBadWords();
 
-		foreach ($words as $word) {
-			if(in_array(strtolower($word), $bad_words)){
-				$new_words[] = str_ireplace(['a', 'i', 'u', 'e', 'o', '4', '1', '3', '0'], '*', $word);
-			}
-			else{
-				$new_words[] = $word;
-			}
-		}
+        $new_words = [];
 
-		return implode(' ', $new_words);
-	}
+        foreach ($words as $word) {
+            if (in_array(strtolower($word), $bad_words)) {
+                $new_words[] = str_ireplace(
+                    ["a", "i", "u", "e", "o", "4", "1", "3", "0"],
+                    "*",
+                    $word
+                );
+            } else {
+                $new_words[] = $word;
+            }
+        }
 
-	public static function getBadWords()
-	{
-		return array_map(function($item){
-			return strtolower(trim($item));
-		}, explode("\n", file_get_contents(__DIR__ . '/badwords.txt')));
-	}
+        return implode(" ", $new_words);
+    }
+
+    public static function getBadWords()
+    {
+        return array_map(function ($item) {
+            return strtolower(trim($item));
+        }, explode("\n", file_get_contents(__DIR__ . "/badwords.txt")));
+    }
+
+    public static function getBadPhrases()
+    {
+        $bad_words = self::getBadWords();
+
+        $bad_phrases = [];
+
+        foreach ($bad_words as $bad_word) {
+            $words = explode(" ", $bad_word);
+
+            if (count($words) > 1) {
+                $bad_phrases[] = $bad_word;
+            }
+        }
+
+        return $bad_phrases;
+    }
 }
